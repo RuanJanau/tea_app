0f65fdf2daa2f5534b29797ea978973489f2af81
RhuanJanau <rhuanjanau531@gmail.com>
2022-09-19 20:08:52 -0300

    add Moludar

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'src/modules/app_module.dart';
+import 'src/modules/app_widget.dart';
 
-void main(){
+void main() {
   return runApp(ModularApp(module: AppModule(), child: AppWidget()));
 }
-
-class AppWidget extends StatelessWidget {
-  Widget build(BuildContext context){
-    return MaterialApp.router(
-      title: 'My Smart App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      routeInformationParser: Modular.routeInformationParser,
-      routerDelegate: Modular.routerDelegate,
-    ); //added by extension 
-  }
-}
-
-class AppModule extends Module {
-  @override
-  List<Bind> get binds => [];
-
-  @override
-  List<ModularRoute> get routes => [
-    ChildRoute('/', child: (context, args) => HomePage()),
-  ];
-}
-
-class HomePage extends StatelessWidget {
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(title: Text('Home Page')),
-      body: Center(
-        child: Text('This is initial page'),
-      ),
-    );
-  }
-}
-
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/RuanJanau/tea_app.git
-git push -u origin main
---
+++ lib/src/modules/home/view/screen_home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ScreenHome extends StatefulWidget {
+  const ScreenHome({super.key});
+
+  @override
+  State<ScreenHome> createState() => _ScreenHomeState();
+}
+
+class _ScreenHomeState extends State<ScreenHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
