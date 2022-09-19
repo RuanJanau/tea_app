67f64a8dc332b67bda23238cf47a8ce9c1d8ddf0
RhuanJanau <rhuanjanau531@gmail.com>
2022-09-19 20:05:21 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+void main(){
+  return runApp(ModularApp(module: AppModule(), child: AppWidget()));
+}
+
+class AppWidget extends StatelessWidget {
+  Widget build(BuildContext context){
+    return MaterialApp.router(
+      title: 'My Smart App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
+    ); //added by extension 
+  }
+}
+
+class AppModule extends Module {
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [
+    ChildRoute('/', child: (context, args) => HomePage()),
+  ];
+}
+
+class HomePage extends StatelessWidget {
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page')),
+      body: Center(
+        child: Text('This is initial page'),
+      ),
+    );
+  }
+}
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/RuanJanau/tea_app.git
+git push -u origin main
